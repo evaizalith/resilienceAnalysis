b1bcdcb4d6ffd0fdba1a04450888d26c47eeba76
Eva <53011549+FoundIzalith@users.noreply.github.com>
2023-07-11 11:01:07 -0400

    Update compileAndRun.sh

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,11 +1,11 @@
-OPT_LEVEL1="O0" # Optimization level for clang
-OPT_LEVEL2="00" # Optimization level for LLTFI
+OPT_LEVEL="O0" # Optimization level for clang
+NUM_RUNS="10" # Number of runs for LLTFI
 
 rm -rf ./llfi*
 fname=$1
 
 echo "Compiling..."
-clang++ -w -emit-llvm -fno-unroll-loops -lstdc++ -fno-use-cxa-atexit -$OPT_LEVEL1 -S *.cc
+clang++ -w -emit-llvm -fno-unroll-loops -lstdc++ -fno-use-cxa-atexit -$OPT_LEVEL -S *.cc
 echo "Calling llvm-link..."
 $HOME/llvm-project/build/bin/llvm-link -o "$fname.ll" -S *.ll
 
@@ -16,9 +16,9 @@ python3 $LLFI_BUILD_ROOT/bin/instrument.py -lstdc++ --readable "$fname.ll"
 
 shift
 echo "Profiling..."
-python3 $LLFI_BUILD_ROOT/bin/profile.py ./llfi/"$fname-profiling.exe" $OPT_LEVEL2 $@
+python3 $LLFI_BUILD_ROOT/bin/profile.py ./llfi/"$fname-profiling.exe" $NUM_RUNS $@
 
 echo "Injecting faults..."
-python3 $LLFI_BUILD_ROOT/bin/injectfault.py ./llfi/"$fname-faultinjection.exe" $OPT_LEVEL2 $@
+python3 $LLFI_BUILD_ROOT/bin/injectfault.py ./llfi/"$fname-faultinjection.exe" $NUM_RUNS $@
 
 echo "All done!" 
